6162c64924dc8cad1c3fe381285962d5c6c45719
Hemangi24322 <charelhemangi24322@gmail.com>
2025-06-26 17:22:05 +0530

    cors check

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,10 +20,7 @@ const connectDB = require('./config/database');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
